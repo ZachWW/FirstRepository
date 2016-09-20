7b38ff8fd1eb978bbbf1f5d38add49be00e9d6fa
zacharywaines <zacharywaines@AA0508-20.labs.sce.carleton.ca>
2016-09-20 12:00:31 -0400

    Fixed up the code

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,13 +1,17 @@
 
 import java.util.ArrayList;
 
-public class AddressBook extends BuddyInfo {
+public class AddressBook {
 
-	private ArrayList<BuddyInfo> buddyList = new ArrayList<BuddyInfo>();
+	private ArrayList<BuddyInfo> buddyList;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is address");
+	}
+	
+	public AddressBook() {
+		buddyList = new ArrayList<BuddyInfo>();
 	}
 	
 	public void addBuddy(BuddyInfo buddy) {
